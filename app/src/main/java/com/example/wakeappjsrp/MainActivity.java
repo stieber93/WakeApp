a06a44caa9b3525007066bc0c4a9a1254867a505
gero-GreeN <gerolp8@gmail.com>
2022-05-12 11:17:51 +0200

    Abfrage der Inputfelder ermöglicht

--- app/src/main/java/com/example/wakeappjsrp/MainActivity.java
+++ app/src/main/java/com/example/wakeappjsrp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.wakeappjsrp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test
     }
+
+    public void changeResultTime(View view) {
+        EditText inputZeit = (EditText) findViewById(R.id.inputUhrzeit);
+        EditText inputFahrtzeit = (EditText) findViewById(R.id.inputFahrtzeit);
+        EditText inputZZF = (EditText) findViewById(R.id.inputZeitZumFertigmachen);
+        String inputUhrzeitValue = inputZeit.getText().toString();
+        String inputFahrtzeitValue = inputFahrtzeit.getText().toString();
+        String inputZZFValue = inputZZF.getText().toString();
+        Log.d("resultZeit", inputUhrzeitValue);
+        Log.d("resultFahrtzeit", inputFahrtzeitValue);
+        Log.d("resultZZF", inputZZFValue);
+    }
+
+
 }
