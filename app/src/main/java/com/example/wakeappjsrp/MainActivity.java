302d233674e8d50c4ccdb23f834280e2ae2363ef
EmeraldMageHD <EmeraldMageHD@gmail.com>
2022-05-12 11:28:32 +0200

    TimePicker24H aber no pop up

--- app/src/main/java/com/example/wakeappjsrp/MainActivity.java
+++ app/src/main/java/com/example/wakeappjsrp/MainActivity.java
@@ -2,18 +2,36 @@ package com.example.wakeappjsrp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
-    TimePicker picker;
-    Button btnGet;
-    TextView tvw;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker1);
+        timePicker.setIs24HourView(true);
+        timePicker.setEnabled(false);
+        EditText showTimePicker = (EditText) findViewById(R.id.inputUhrzeit);
+        showTimePicker.setOnClickListener((View.OnClickListener) this);
+        //Test
+        //test Nick
+        //Test Domi
+    }
+    public void onClick(final View e) {
+        View showTimePicker;
+        if(e == showTimePicker){
+        TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker1);
+        timePicker.setEnabled(true);
+        }
+    }
+
 
 
 }
