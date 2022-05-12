be55ec0ebcb5fcad2eae9d1f6ee9ff24b9dbc905
EmeraldMageHD <EmeraldMageHD@gmail.com>
2022-05-12 09:54:41 +0200

    TestTimePicker

--- app/src/main/java/com/example/wakeappjsrp/MainActivity.java
+++ app/src/main/java/com/example/wakeappjsrp/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.wakeappjsrp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //Test
-        //Test Domi
-    }
+    TimePicker picker;
+    Button btnGet;
+    TextView tvw;
+
+
 }
