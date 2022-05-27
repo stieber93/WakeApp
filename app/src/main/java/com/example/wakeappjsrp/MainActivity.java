3dd94f11a081972939ec9bbeb81281fde72001de
EmeraldMageHD <EmeraldMageHD@gmail.com>
2022-05-27 12:30:49 +0200

    Funktionierender Time Picker

--- app/src/main/java/com/example/wakeappjsrp/MainActivity.java
+++ app/src/main/java/com/example/wakeappjsrp/MainActivity.java
@@ -1,37 +1,51 @@
 package com.example.wakeappjsrp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.TimePickerDialog;
-import android.icu.util.Calendar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    TimePickerDialog picker;
+    EditText eText;
+    Button btnGet;
+    TextView tvw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker1);
-        timePicker.setIs24HourView(true);
-        timePicker.setEnabled(false);
-        EditText showTimePicker = (EditText) findViewById(R.id.inputUhrzeit);
-        showTimePicker.setOnClickListener((View.OnClickListener) this);
-        //Test
-        //test Nick
-        //Test Domi
+        tvw=(TextView)findViewById(R.id.textView1);         // Dies ist nur eine Ausgabe als Text nach drücke des Buttons
+        eText=(EditText) findViewById(R.id.inputUhrzeit);  //Hier als ID die Textbox auswählen
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int hour = cldr.get(Calendar.HOUR_OF_DAY);
+                int minutes = cldr.get(Calendar.MINUTE);
+                // time picker dialog
+                picker = new TimePickerDialog(MainActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker tp, int sHour, int sMinute) {
+                                eText.setText(sHour + ":" + sMinute);
+                            }
+                        }, hour, minutes, true);
+                picker.show();
+            }
+        });
+        btnGet=(Button)findViewById(R.id.button1);            //Dies ist nur für den Zeit ausgeben Button
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvw.setText("Selected Time: "+ eText.getText());
+            }
+        });
     }
-    public void onClick(final View e) {
-        View showTimePicker;
-        if(e == showTimePicker){
-        TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker1);
-        timePicker.setEnabled(true);
-        }
-    }
-
-
-
 }
